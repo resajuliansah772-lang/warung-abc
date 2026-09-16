a5f78d75d94c44f1223d79dde6ba3e976ba798f4
natiyaaaa <resajuliansah772@gmail.com>
2026-09-16 11:03:43 +0700

    selesai

--- proses_simpan_transaksi.php
+++ proses_simpan_transaksi.php
@@ -1,64 +1,57 @@
-<?php
-//proses_simpan_transaksi.php
-include 'includes/cek_session.php';
-include 'config/koneksi.php';
+<?php 
+//proses_simpan_transaksi.php 
+include 'includes/cek_session.php'; 
+include 'config/koneksi.php'; 
+ 
+if (empty($_SESSION['keranjang'])) { 
+    $_SESSION['pesan_error'] = 'keranjang masih kosong!'; 
+    header('Location: transaksi.php'); 
+    exit; 
+} 
+ 
+$id_kasir = $_SESSION['id_user']; 
 
-if (empty($_SESSION['keranjang'])) {
-    $_SESSION['pesan_error'] = 'keranjang masih kosong!';
+if (empty($_POST['id_pelanggan'])) {
+    $_SESSION['pesan_error'] = 'Silakan pilih pelanggan terlebih dahulu!';
     header('Location: transaksi.php');
     exit;
 }
 
-$id_kasir = $_SESSION['id_user'];
-$id_pelanggan = !empty($_POST['id_pelanggan']) ? (int) $_POST['id_pelanggan'] : null;
-$id_pelanggan_sql = $id_pelanggan === null ? 'NULL' : "'$id_pelanggan'";
-
-$no_transaksi = 'TRX-' . date('YmdHis');
-$tanggal = date('Y-m-d H:i:s');
-$total = 0;
-
-foreach ($_SESSION['keranjang'] as $item) {
-    $total += $item['subtotal'];
-}
-
-$sql = "INSERT INTO tbl_transaksi (no_transaksi, tanggal, id_kasir, id_pelanggan, total_bayar)";
-$sql .= "VALUES ('$no_transaksi', '$tanggal', '$id_kasir', $id_pelanggan_sql, '$total')";
-
-mysqli_query($koneksi, $sql);
-$id_transakasi = mysqli_insert_id($koneksi);
-
-$id_kasir = $_SESSION['id_user'];
-$no_transaksi = 'TRX-' . date('YmdHis');
-$tanggal = date('Y-m-d H:i:s');
-
-$total = 0;
-foreach ($_SESSION['keranjang'] as $item) {
-    $total += $item['subtotal'];
-}
-
-$sql = "INSERT INTO tbl_transaksi (no_transaksi, tanggal, id_kasir, id_pelanggan, total_bayar) VALUES ('$no_transaksi', '$tanggal', '$id_kasir', NULL, '$total')";
-mysqli_query($koneksi, $sql);
-
-$id_transakasi = mysqli_insert_id($koneksi);
-
-foreach ($_SESSION['keranjang'] as $id_barang => $item) {
-    $jumlah = $item['jumlah'];
-    $subtotal = $item['subtotal'];
-
-    $detail = "INSERT INTO tbl_detail_transaksi (id_transaksi, id_barang, jumlah, subtotal) VALUES ('$id_transaksi','$id_barang','$jumlah','$subtotal')";
-    mysqli_query($koneksi, $detail);
-
-    $update_stok = "UPDATE tbl_barang SET stok = stok - $jumlah WHERE id_barang = '$id_barang'";
-    mysqli_query($koneksi, $update_stok);
-}
-
-$waku = date('Y-m-d H:i:s');
-$aktivitas = "transaksi: $no_transaksi";
-$log ="INSERT INTO tbl_log (id_user, aktivitas, waktu) VALUES ('$id_kasir', '$aktivitas', '$waku')";
-mysqli_query($koneksi, $log);
-
-unset($_SESSION['keranjang']);
-
-header('Location: riwayat_transaksi.php');
-exit;
+$id_pelanggan = (int) $_POST['id_pelanggan'];
+$id_pelanggan_sql = "'$id_pelanggan'";
+ 
+$no_transaksi = 'TRX-' . date('YmdHis'); 
+$tanggal = date('Y-m-d H:i:s'); 
+$total = 0; 
+ 
+foreach ($_SESSION['keranjang'] as $item) { 
+    $total += $item['subtotal']; 
+} 
+ 
+$sql = "INSERT INTO tbl_transaksi (no_transaksi, tanggal, id_kasir, id_pelanggan, total_bayar)"; 
+$sql .= " VALUES ('$no_transaksi', '$tanggal', '$id_kasir', $id_pelanggan_sql, '$total')"; 
+ 
+mysqli_query($koneksi, $sql); 
+$id_transaksi = mysqli_insert_id($koneksi); 
+ 
+foreach ($_SESSION['keranjang'] as $id_barang => $item) { 
+    $jumlah = $item['jumlah']; 
+    $subtotal = $item['subtotal']; 
+ 
+    $detail = "INSERT INTO tbl_detail_transaksi (id_transaksi, id_barang, jumlah, subtotal) VALUES ('$id_transaksi','$id_barang','$jumlah','$subtotal')"; 
+    mysqli_query($koneksi, $detail); 
+ 
+    $update_stok = "UPDATE tbl_barang SET stok = stok - $jumlah WHERE id_barang = '$id_barang'"; 
+    mysqli_query($koneksi, $update_stok); 
+} 
+ 
+$waku = date('Y-m-d H:i:s'); 
+$aktivitas = "transaksi: $no_transaksi"; 
+$log = "INSERT INTO tbl_log (id_user, aktivitas, waktu) VALUES ('$id_kasir', '$aktivitas', '$waku')"; 
+mysqli_query($koneksi, $log); 
+ 
+unset($_SESSION['keranjang']); 
+ 
+header('Location: riwayat_transaksi.php'); 
+exit; 
 ?>
